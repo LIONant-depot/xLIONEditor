02d5bf7512a9a09d675afbc899fe7b0012d088c5
Jack <x@x.com>
2021-10-23 03:18:28 +0800

    wip

---
+++ src/main.cpp
@@ -0,0 +1,65 @@
+#define PROPERTY_EDITOR
+#include "../dependencies/xEditor/Src/xeditor.h"
+#include "../../dependencies/xEditor/dependencies/xgpu/src/xGPU.h"
+#include "../../dependencies/xEditor/dependencies/xgpu/tools/xgpu_imgui_breach.h"
+#include "../dependencies/properties/src/Examples/ImGuiExample/ImGuiPropertyInspector.cpp"
+
+//------------------------------------------------------------------------------------------------
+
+static
+void DebugMessage(std::string_view View)
+{
+    printf("%s\n", View.data());
+}
+
+//------------------------------------------------------------------------------------------------
+
+int main()
+{
+    xgpu::instance Instance;
+    if (auto Err = xgpu::CreateInstance(Instance, { .m_bDebugMode = false, .m_bEnableRenderDoc = false, .m_pLogErrorFunc = DebugMessage, .m_pLogWarning = DebugMessage }); Err)
+        return xgpu::getErrorInt(Err);
+
+    xgpu::device Device;
+    if (auto Err = Instance.Create(Device); Err)
+        return xgpu::getErrorInt(Err);
+
+    xgpu::window MainWindow;
+    if (auto Err = Device.Create(MainWindow, {}); Err)
+        return xgpu::getErrorInt(Err);
+
+    //
+    // Setup ImGui
+    //
+    xgpu::tools::imgui::CreateInstance(MainWindow);
+
+    //
+    // Main loop
+    //
+    while (Instance.ProcessInputEvents())
+    {
+        if (xgpu::tools::imgui::BeginRendering())
+            continue;
+
+        //
+        // Show ImGui demo
+        // 
+        static bool show_demo_window = true;
+        ImGui::ShowDemoWindow(&show_demo_window);
+
+     //   DrawPropertyWindow();
+
+        //
+        // Render
+        //
+        xgpu::tools::imgui::Render();
+
+        //
+        // Pageflip the windows
+        //
+        MainWindow.PageFlip();
+    }
+
+    return 0;
+}
+
