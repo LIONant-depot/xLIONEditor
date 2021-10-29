547ae9ee82eac7dee531a8fbb8055a04719fc472
Jack <x@x.com>
2021-10-30 04:37:45 +0800

    wip

--- src/Editors/SceneED/Details/xlioned_sceneed_panel.cpp
+++ src/Editors/SceneED/Details/xlioned_sceneed_panel.cpp
@@ -4,7 +4,7 @@
 
 namespace xlioned::sceneed {
 
-    xeditor::panel::details::type_harness<panel, explorer::panel, properties::panel> s_Type{ panel::class_name_v };
+    xeditor::panel::details::type_harness<panel, explorer::panel, properties::panel> s_Type{ panel::class_name_v, xeditor::panel::type::flags{ .m_bDisplayOnEmptyProject = true } };
 
     //-------------------------------------------------------------------------
 
@@ -26,6 +26,24 @@ namespace xlioned::sceneed {
 
     void panel::onRender(void)
     {
-    
+        ImGui::PushStyleVar(ImGuiStyleVar_ChildRounding, 6.0f);
+        ImGui::SetNextWindowBgAlpha(0.35f); // Transparent background
+        SetNextWindowPosRelativeToCenterDock(position_type::TOP_MIDDLE, { 0,2 });
+        ImGui::BeginChild( "Menu", {25*4, 35}, true, ImGuiWindowFlags_NoScrollbar );
+     //   ImGui::SetCursorPosX(ImGui::GetCursorPosX() + (float)5);
+     //   ImGui::SetCursorPosY(ImGui::GetCursorPosY() + (float)3);
+        ImGui::Button("X");
+
+        ImGui::SameLine();
+        ImGui::Button("X");
+
+        ImGui::SameLine();
+        ImGui::Button("X");
+
+        ImGui::SameLine();
+        ImGui::Button("X");
+
+        ImGui::PopStyleVar();
+        ImGui::EndChild();
     }
 }
